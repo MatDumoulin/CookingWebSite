3b0ddc5edc2e5144f411cd12c8fca46547f604fe
Mathieu Dumoulin <mathieu.dumoulin@outlook.com>
2017-09-05 15:35:38 -0400

    App now shares server with the api if in prod mode. To enable it, environment variable ENV must be set to prod. If not set to prod, app runs as it used before. First: npm run server. Second: npm start.

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const routerManager = require('./api/index');
 
 const app = express();
 
-//const url = 'mongodb://mycookingbook:`p:cXPN9$.Q)8_#$@' + (process.env.MYCOOKINGAPPIP || "64.137.201.197") + ':27015/mycookingbook';
 const url = 'mongodb://localhost:27017/easycooking';
 
 app.set('port', (process.env.PORT || 5000));
@@ -19,10 +18,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 // Directory routes to hide the structure of the project.
 app.use(express.static(__dirname));
-/*app.use('/ng-infinite-scroll', express.static(__dirname + '/node_modules/ng-infinite-scroll/build/'));*/
 
-// views is directory for all template files
-//app.set('views', __dirname + '/src/views');
 
 // Using the connection pool provided by the MongoClient driver to manage database connections.
 // To make sure that we have it set up before we render the website, we are setting it in the
@@ -38,3 +34,9 @@ mongoClient.connect(url, function(err, database) {
       console.log('Node app is running on port', app.get('port'));
     });
 });
+
+if(process.env.ENV == 'prod') {
+  app.get('/', function(request, response) {
+    response.sendFile(__dirname + '/dist/index.html');
+  });
+}
