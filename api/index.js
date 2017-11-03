a5fdf3e8df37a384eb90185c3aa9b65421803893
Mathieu Dumoulin <mathieu.dumoulin@outlook.com>
2017-11-03 09:16:13 -0400

    Allowing Delete request to the api on prod server.

--- api/index.js
+++ api/index.js
@@ -19,6 +19,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 // Enabling CORS as we want to communicate with the server.
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
