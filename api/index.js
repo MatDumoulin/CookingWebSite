35b848eec49162eda48ecf0a841fc058eff0f92c
Mathieu Dumoulin <mathieu.dumoulin@outlook.com>
2018-01-07 16:08:52 -0500

    Restructuring the file distribution architecture of the server.

--- api/index.js
+++ api/index.js
@@ -7,19 +7,14 @@ const routerManager = require('./route-manager');
 
 const app = express();
 
-// Allowing bodies of up to 50mb. (for image upload)
+// Needed in order to read the body of the requests.
+// Allowing bodies of up to 10mb. (for image upload)
 app.use(bodyParser.json({limit: '10mb'}));
 app.use(bodyParser.urlencoded({limit: '10mb', extended: true}));
 
 const url = 'mongodb://mycookingbook:~c2[hW-F#^`GpPrU@localhost:27017/easycooking';
 
 app.set('port', (process.env.PORT || 5000));
-
-// Needed in order to read the body of the requests.
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-    extended: true
-}));
 
 // Enabling CORS as we want to communicate with the server.
 app.use(function(req, res, next) {
--- prod-server.js
+++ prod-server.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 4200));
 
 
 // Directory routes to hide the structure of the project.
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/server'));
 
 // Enabling CORS as we want to communicate with the API.
 app.use(function(req, res, next) {
@@ -22,5 +22,5 @@ app.listen(app.get('port'), function() {
 // Always send the index.html file. Angular's routing is handling the different
 // url. That way, the page reload works when done with the browser.
 app.get('/*', function(request, response, next) {
-  response.sendFile(__dirname + '/dist/index.html');
+  response.sendFile(__dirname + '/server/dist/index.html');
 });
