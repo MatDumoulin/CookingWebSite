cc65c616f34d3e1590e1c397ea4c8a94fb1fd1a0
Mathieu Dumoulin <mathieu.dumoulin@outlook.com>
2018-02-04 19:13:58 -0500

    The recipe types from the database now are limited to the ones the user created.

--- server/api/routes/recipes_get_genres.js
+++ server/api/routes/recipes_get_genres.js
@@ -1,13 +1,16 @@
-// Finds all of the recipe genres in the database.
+const getUserId = require('../jwt-reader');
+// Finds all of the recipe genres of the user in the database.
 
 // Params:
 // No parameter is needed.
 function routeFactory(dbColl) {
     return function getRecipeGenres(req, res) {
+        // Getting the user from the jwt
+        const userAuthId = getUserId(req);
         // These variable are used only for clarity purpose. They could have
         // been inline in the 'distinct' call but it wasn't explicit enough.
         const field = 'genre';
-        const condition = {"genre": {$exists: true, $ne: ""}};
+        const condition = {"genre": {$exists: true, $ne: ""}, 'owner': userAuthId};
 
         // Gets all of the recipe genre from the database.
         dbColl.distinct(field, condition, function(err, docs) {
